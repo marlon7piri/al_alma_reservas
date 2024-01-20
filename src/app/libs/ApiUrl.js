41fe142c2034cc58c8d2ba6545ae8f5275bed7b6
marlonrodri7 <marlon7piri@gmail.com>
2024-01-20 09:36:15 -0500

    add new url

--- src/app/libs/ApiUrl.js
+++ src/app/libs/ApiUrl.js
@@ -1,4 +1,4 @@
-const url = 'https://al-alma-reserva.vercel.app/api'
+const url = 'https://al-alma-reservas.vercel.app//api'
  const url2 = 'http://localhost:3000/api'
 
-export const ApiUrl= url2
+export const ApiUrl= url
